7551a052c869b090bc69f1b7d008d0418191fdce
diamond <aaron.diamond2@yahoo.co.nz>
2023-03-01 00:29:05 +1300

    added dummy data for posts

--- src/components/Profile/PostContainers.jsx
+++ src/components/Profile/PostContainers.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiLike, BiShare, BiCommentDetail } from "react-icons/bi";
 import Nature from "../../assets/images/nature.jpg";
+import TestPost2 from "../../assets/images/testpost2.jpg";
 
 const PostContainers = () => {
   return (
@@ -57,7 +58,56 @@ const PostContainers = () => {
           </div>
         </button>
 
-        <div className="post__wrapper"></div>
+        <button className="post-button__wrapper">
+          <div
+            className="post__wrapper"
+            style={{
+              backgroundImage: `url(${TestPost2})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+            }}
+          >
+            <div className="post-content__wrapper">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestiae earum harum omnis quasi obcaecati minus illum laborum
+                commodi, minima laudantium molestias facilis qui exercitationem
+                provident ullam dolorum repellendus perspiciatis sapiente.
+              </p>
+            </div>
+
+            <div className="post-icons__wrapper">
+              <ul className="post-icons">
+                <li>
+                  <button>
+                    <BiLike
+                      size="20px"
+                      style={{ backgroundColor: "transparent" }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <BiCommentDetail
+                      size="20px"
+                      color="blue"
+                      style={{ backgroundColor: "transparent" }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <BiShare
+                      size="20px"
+                      style={{ backgroundColor: "transparent" }}
+                    />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </button>
         <div className="post__wrapper"></div>
         <div className="post__wrapper"></div>
         <div className="post__wrapper"></div>
