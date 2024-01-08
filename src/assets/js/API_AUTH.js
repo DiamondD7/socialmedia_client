6315a54a4b4d0f712bb72094d2adb19d8a3b09e1
diamond <aaron.diamond2@yahoo.co.nz>
2024-01-09 07:24:02 +1300

    implemented the news api in the news section of the feed

---
+++ src/assets/js/API_AUTH.js
@@ -0,0 +1,2 @@
+const BASE_NEWS_URL = 'https://newsapi.org/v2';
+export const LATEST_NEWS = `${BASE_NEWS_URL}/top-headlines?country=nz&apiKey=`; //change the language/region to be dynamic base on the user's location.
--- src/components/Home/Home Components/NewsContainer.jsx
+++ src/components/Home/Home Components/NewsContainer.jsx
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { LATEST_NEWS } from "../../../assets/js/API_AUTH";
 
 import "../../../styles/newscontainerstyles.css";
 const NewsContainer = () => {
+  const [latestNews, setLatestNews] = useState([]);
+
+  useEffect(() => {
+    fetch(`${LATEST_NEWS}${import.meta.env.VITE_NEWS_API_KEY}`, {})
+      .then((res) => res.json())
+      .then((data) => {
+        setLatestNews(data.articles);
+      });
+  }, []);
+
   return (
     <div>
       <div className="news-main-container__wrapper">
         <div className="news-container__wrapper">
           <h2 className="news-container__title__header">News</h2>
           <ul className="news-container__news-title__wrapper">
-            <li>
-              'Guess What?' Stephen Colbert Has News For 'Divorce' Fiend
-              Marjorie Taylor Greene
-            </li>
-            <li>Bizarre sight over Sydney stuns onlookers: 'What is it?'</li>
-            <li>
-              Donald Trump Jr. Gets Scathing Reminder Of His Father's 'Mental
-              Competency'
-            </li>
-            <li>
-              Virginia Democrat Wins Special Election to Replace Late
-              Congressman — and Makes History in the Process
-            </li>
-            <li>
-              Kourtney Kardashian Shares Photos from Romantic 'Snowed-In'
-              Valentine's Day Trip with Husband Travis Barker
-            </li>
-            <li>
-              Black Panther's Letitia Wright Clears Up Comments About Third
-              Movie
-            </li>
+            {latestNews.map((data, index) => (
+              <li key={index}>
+                <a href={data.url} target="_blank">
+                  {data.title}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
